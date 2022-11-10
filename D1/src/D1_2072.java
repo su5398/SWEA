71f345238094f25b82b46f4f5589c4c6185b6a39
유ìsu5398 <su539@naver.com>
2022-11-11 00:46:13 +0900

    2072풀이

--- D1/src/D1_2072.java
+++ D1/src/D1_2072.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class D1_2072 {
+
+    static int T, N, SUM;
+
+    public static void main(String args[]) throws IOException {
+
+       Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+
+            SUM = 0;
+            for(int j = 0; j < 10; j++){
+                N = sc.nextInt();
+                if(N%2 == 1)
+                    SUM += N;
+            }
+
+            System.out.println("#"+(i+1)+" "+SUM);
+        }
+    }
 }
