3f28ddd649fcc314174a5259a6baeab84e90c82b
유ìsu5398 <su539@naver.com>
2023-01-24 23:41:50 +0900

    백준 3425

---
+++ Baekjoon/src/N3425.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class N3425 {
+
+    public static ArrayList<String> list = new ArrayList<>();
+    public static long[] stack = new long[1001];
+    public static int head = 0;
+    public static int MAX = 1000000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true){
+            list.clear();
+            String line = br.readLine();
+
+            if(line.equals("QUIT")){ //기게에 대한 설명 없음
+                break;
+            }
+
+
+        }
+
+
+    }
+
+
+}
